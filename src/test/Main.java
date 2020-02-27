cabe07f8b0cc5bfa84ecf94576c5bc4d945808f3
Fazle Rabbi <frduiit@gmail.com>
2020-02-27 20:07:16 +0600

    ss

--- src/test/Main.java
+++ src/test/Main.java
@@ -3,10 +3,10 @@ package test;
 public class Main {
 	public static void main(String [] args)
 	{
-		FileProcessor fileProcessor = new FileProcessor("/home/rabbi/Desktop/code-comment-data/new");
-		fileProcessor.loadAndSave("tmp.json");
-		fileProcessor.loadAndSave("test.json");
-		fileProcessor.loadAndSave("valid.json");
-		fileProcessor.loadAndSave("train.json");
+		FileProcessor fileProcessor = new FileProcessor("/home/rabbi/Desktop/code-comment-data");
+//		fileProcessor.loadAndSave("tmp.json");
+//		fileProcessor.loadAndSave("test.json");
+//		fileProcessor.loadAndSave("valid.json");
+		fileProcessor.loadAndSave("all_pairs.json");
 	}
 }
